4bccc6f24d6acd8f5f2d40c09111515a7b6360e6
king-kazu39 <magmanakazato@gmail.com>
2022-12-03 19:54:18 +0900

    typescriptインストールのコマンド追加

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,9 @@ asdf install
 # firebaseコマンドを有効にします
 npm install -g firebase-tools
 
-functionsディレクトリに移動してfunctionsをビルドする
-ビルドが終わったらプロジェクトルートに戻る
+# functionsディレクトリに移動してfunctionsをビルドする
+# ビルドが終わったらプロジェクトルートに戻る
 cd functions
+npm install typescript
 npm run build 
 cd ..
